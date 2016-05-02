9bcbe8f0b042352ecbf43ee1059545013309ffb5
DryRun <david.renhwa.yu@gmail.com>
2016-05-03 01:41:26 +0200

    Add outputFile option

--- test/InclusiveBHistograms_cfg.py
+++ test/InclusiveBHistograms_cfg.py
@@ -3,18 +3,22 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 
 options = VarParsing.VarParsing()
 options.register('inputFiles', 
-	'', 
+	'/uscms/home/dryu/eosdir/BJetPlusX/QCDBEventTree_BJetPlusX_Run2012B_v1_3/160429_121519/0000/QCDBEventTree_567.root',
 	VarParsing.VarParsing.multiplicity.singleton,
 	VarParsing.VarParsing.varType.string,
 	"List of input files"
 )
+options.register('outputFile', 
+	'InclusiveBHistograms.root', 
+	VarParsing.VarParsing.multiplicity.singleton,
+	VarParsing.VarParsing.varType.string,
+	"Output file"
+)
 options.parseArguments()
 
 
-
-
 process = cms.Process("myprocess")
-process.TFileService=cms.Service("TFileService",fileName=cms.string('InclusiveBHistograms.root'))
+process.TFileService=cms.Service("TFileService",fileName=cms.string(options.outputFile))
 
 ##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
@@ -24,10 +28,7 @@ process.source = cms.Source("EmptySource")
 
 ##-------------------- User analyzer  --------------------------------
 process.inclusive    = cms.EDAnalyzer('InclusiveBHistograms',
-	file_names             = cms.vstring(
-	'/uscms/home/dryu/eosdir/BJetPlusX/QCDBEventTree_BJetPlusX_Run2012B_v1_3/160429_121519/0000/QCDBEventTree_567.root',
-	'/uscms/home/dryu/eosdir/BJetPlusX/QCDBEventTree_BJetPlusX_Run2012B_v1_3/160429_121519/0000/QCDBEventTree_585.root',
-	),
+	file_names             = cms.vstring(options.inputFiles),
 	tree_name              = cms.string('ak5/ProcessedTree'),
 	trigger_histogram_name = cms.string('ak5/TriggerNames'),
 	#triggers               = cms.vstring('HLT_DiJet80Eta2p6_BTagIP3DFastPVLoose_v2:L1_DoubleJetC36', 'HLT_DiJet80Eta2p6_BTagIP3DFastPVLoose_v3:L1_DoubleJetC36', 'HLT_DiJet80Eta2p6_BTagIP3DFastPVLoose_v4:L1_DoubleJetC36', 'HLT_DiJet80Eta2p6_BTagIP3DFastPVLoose_v5:L1_DoubleJetC36', 'HLT_DiJet80Eta2p6_BTagIP3DFastPVLoose_v7:L1_DoubleJetC36'),
