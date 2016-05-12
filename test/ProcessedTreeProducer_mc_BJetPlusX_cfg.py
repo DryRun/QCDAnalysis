9c688e18d15db4a151cf17934e888894dea9accf
DryRun <david.renhwa.yu@gmail.com>
2016-05-12 18:30:26 +0200

    dataset=file:... now registers a local file

--- test/ProcessedTreeProducer_mc_BJetPlusX_cfg.py
+++ test/ProcessedTreeProducer_mc_BJetPlusX_cfg.py
@@ -74,7 +74,9 @@ process.maxEvents = cms.untracked.PSet(
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 #############   Define the source file ###############
 
-if options.dataset == "/QCD_Pt-600to800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM":
+if "file:" in options.dataset:
+    input_file_vstring = cms.untracked.vstring(options.dataset)
+elif options.dataset == "/QCD_Pt-600to800_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v2/AODSIM":
     input_file_vstring = cms.untracked.vstring('/store/mc/Summer12_DR53X/QCD_Pt-600to800_TuneZ2star_8TeV_pythia6/AODSIM/PU_S10_START53_V7A-v2/00000/00BA1E5C-2008-E211-9F96-002618943951.root')
 elif options.dataset == "/RSGravitonToBBbar_M-700_TuneZ2star_8TeV-pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM":
     input_file_vstring = cms.untracked.vstring('/store/mc/Summer12_DR53X/RSGravitonToBBbar_M-700_TuneZ2star_8TeV-pythia6/AODSIM/PU_S10_START53_V7A-v1/00000/86359ACA-8110-E211-B7C5-BCAEC50971E3.root')
