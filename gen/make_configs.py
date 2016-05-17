1915037eb48cb999a0a2b349aace42697961fbf7
DryRun <david.renhwa.yu@gmail.com>
2016-05-17 19:51:19 +0200

    AODSIM output to save space

--- gen/make_configs.py
+++ gen/make_configs.py
@@ -44,7 +44,8 @@ def MakeFullSimCfgs(p_model, p_mass_point):
 
 	for stage in stages:
 		os.system(commands[stage])
-		AddSubjobOption(GetConfigPath(p_model, p_mass_point, stage, "FULLSIM"), GetOutputTag(p_model, p_mass_point, stage, "FULLSIM"))
+		do_add_input_files = (stage != "GENSIM")
+		AddSubjobOption(GetConfigPath(p_model, p_mass_point, stage, "FULLSIM"), GetOutputTag(p_model, p_mass_point, stage, "FULLSIM"), add_subjob=True, add_input_files=do_add_input_files)
 
 #def MakeFullSimCfg(p_cff, p_cfg, p_output_tag):
 #	cfg_GENSIM = p_cfg.replace("@STAGE@", "GENSIM")
@@ -66,10 +67,15 @@ def MakeFullSimCfgs(p_model, p_mass_point):
 #	AddSubjobOption(cfg_RECO)
 
 def MakeFastSimCfg(p_model, p_mass_point):
-	command = "cmsDriver.py " + GetSimulationCFF(p_model, p_mass_point).replace("$CMSSW_BASE/src/", "") + " --python_filename " + GetConfigPath(p_model, p_mass_point, "RECOSIM", "FASTSIM") + " --fileout file:" + GetOutputTag(p_model, p_mass_point, "RECOSIM", "FASTSIM") + ".root --step GEN,FASTSIM,HLT:7E33v2 --mc --eventcontent RECOSIM --datatier GEN-SIM-DIGI-RECO --pileup 2012_Startup_inTimeOnly --geometry DB --conditions auto:mc --beamspot Realistic8TeVCollision --no_exec -n 1000"
-	print command
-	os.system(command)
-	AddSubjobOption(GetConfigPath(p_model, p_mass_point, "RECOSIM", "FASTSIM"), GetOutputTag(p_model, p_mass_point, "RECOSIM", "FASTSIM"))
+	stages = ["AODSIM"] # RECOSIM?
+	n_events = 5000
+	for stage in stages:
+		# RECOSIM output is large (~2 MB per event). Not reasonable for storage, well, anywhere.
+		#command = "cmsDriver.py " + GetSimulationCFF(p_model, p_mass_point).replace("$CMSSW_BASE/src/", "") + " --python_filename " + GetConfigPath(p_model, p_mass_point, "RECOSIM", "FASTSIM") + " --fileout file:" + GetOutputTag(p_model, p_mass_point, "RECOSIM", "FASTSIM") + ".root --step GEN,FASTSIM,HLT:7E33v2 --mc --eventcontent RECOSIM --datatier GEN-SIM-DIGI-RECO --pileup 2012_Startup_inTimeOnly --geometry DB --conditions auto:mc --beamspot Realistic8TeVCollision --no_exec -n 5000"
+		command = "cmsDriver.py " + GetSimulationCFF(p_model, p_mass_point).replace("$CMSSW_BASE/src/", "") + " --python_filename " + GetConfigPath(p_model, p_mass_point, stage, "FASTSIM") + " --fileout file:" + GetOutputTag(p_model, p_mass_point, stage, "FASTSIM") + ".root --step GEN,FASTSIM,HLT:7E33v2 --mc --eventcontent " + stage + " --datatier GEN-SIM-DIGI-RECO --pileup 2012_Startup_inTimeOnly --geometry DB --conditions auto:mc --beamspot Realistic8TeVCollision --no_exec -n 100"
+		print command
+		os.system(command)
+		AddSubjobOption(GetConfigPath(p_model, p_mass_point, stage, "FASTSIM"), GetOutputTag(p_model, p_mass_point, "RECOSIM", "FASTSIM"))
 
 #def MakeFastSimCfg(p_cff, p_cfg, p_output_tag):
 #	cfg_FASTSIM = p_cfg.replace("@STAGE@", "FASTSIM")
@@ -81,13 +87,13 @@ def MakeFastSimCfg(p_model, p_mass_point):
 #cmsDriver.py PYTHIA6_Bd2Psi2SKpi_TuneZ2star_8TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.py --step GEN,SIM,DIGI,L1,DIGI2RAW,HLT:7E33v2 --beamspot [[Realistic8TeVCollision][Realistic8TeVCollision]] --conditions START53_V7C::All --pileup [[NoPileUp][NoPileUp]] --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM 
 
 #cmsDriver.py PYTHIA6_Bd2Psi2SKpi_TuneZ2star_8TeV_cff_py_RAW2DIGI_L1Reco_RECO.py --step [[RAW2DIGI][RAW2DIGI]],L1Reco,RECO --conditions START53_V19F::All --datamix NODATAMIXER --eventcontent RECOSIM --datatier GEN-SIM-RECO
-def AddSubjobOption(p_cfg, p_output_tag):
+def AddSubjobOption(p_cfg, p_output_tag, add_subjob = False, add_input_files = False):
 	os.system("mv " + p_cfg + " " + p_cfg + ".bak")
 	old = open(os.path.expandvars(p_cfg + ".bak"), 'r')
 	new = open(os.path.expandvars(p_cfg), 'w')
 	for line in old:
-		#print line
-		if "import FWCore.ParameterSet.Config as cms" in line:
+		
+		if "import FWCore.ParameterSet.Config as cms" in line: # Add options
 			# Add VarParsing
 			new.write(line)
 			new.write("import FWCore.ParameterSet.VarParsing as VarParsing\n")
@@ -95,15 +101,23 @@ def AddSubjobOption(p_cfg, p_output_tag):
 			new.write("# Options\n")
 			new.write("#------------------------------------------------------------------------------------\n")
 			new.write("options = VarParsing.VarParsing()\n")
-			new.write("options.register('subjob',\n")
-			new.write("\t0, #default value\n")
-			new.write("\tVarParsing.VarParsing.multiplicity.singleton,\n")
-			new.write("\tVarParsing.VarParsing.varType.int,\n")
-			new.write("\t\"Subjob index, also used for random seed\"\n")
-			new.write(")\n")
+			if add_subjob:
+				new.write("options.register('subjob',\n")
+				new.write("\t0, #default value\n")
+				new.write("\tVarParsing.VarParsing.multiplicity.singleton,\n")
+				new.write("\tVarParsing.VarParsing.varType.int,\n")
+				new.write("\t\"Subjob index, also used for random seed\"\n")
+				new.write(")\n")
+			if add_input_files:
+				new.write("options.register('inputFiles',\n")
+				new.write("\t[], #default value\n")
+				new.write("\tVarParsing.VarParsing.multiplicity.list,\n")
+				new.write("\tVarParsing.VarParsing.varType.string,\n")
+				new.write("\t\"Input files\"\n")
+				new.write(")\n")
 			new.write("options.parseArguments()\n")
-		elif "# Path and EndPath definitions" in line:
-			# Random number seed
+		elif "# Path and EndPath definitions" in line and add_subjob:
+			# Add random number seed
 			new.write("\n")
 			new.write("RandomNumberGeneratorService = cms.Service(\"RandomNumberGeneratorService\",\n")
 			new.write("    generator = cms.PSet(\n")
@@ -112,13 +126,16 @@ def AddSubjobOption(p_cfg, p_output_tag):
 			new.write("    )\n")
 			new.write(")\n\n")
 			new.write(line)
-		elif p_output_tag in line and "fileName = cms.untracked.string":
+		elif p_output_tag in line and "fileName = cms.untracked.string" in line and add_subjob:
+			# Add subjob index to output filenames
 			new.write(line.replace(".root'", ".root.' + str(options.subjob)"))
+		elif "fileNames = cms.untracked.vstring" in line and add_input_files:
+			# Replace input files with option
+			new.write("\tfileNames = cms.untracked.vstring(options.inputFiles)\n")
 		else:
 			new.write(line)
 	old.close()
 	new.close()
-
 
 def MakeCrabCfg(p_cfg):
 	pass
@@ -143,9 +160,9 @@ if __name__ == "__main__":
 			) for model in ["Hbb", "RSG"] for mass_point in mass_points["FASTSIM"]
 		)
 	if args.cfg:
-		Parallel(n_jobs=4)(
-			delayed(MakeFullSimCfgs)(model, mass_point) for model in ["Hbb", "RSG"] for mass_point in mass_points["FULLSIM"]
-		)
+		#Parallel(n_jobs=4)(
+		#	delayed(MakeFullSimCfgs)(model, mass_point) for model in ["Hbb", "RSG"] for mass_point in mass_points["FULLSIM"]
+		#)
 
 		Parallel(n_jobs=4)(
 			delayed(MakeFastSimCfg)(model, mass_point) for model in ["Hbb", "RSG"] for mass_point in mass_points["FASTSIM"]
