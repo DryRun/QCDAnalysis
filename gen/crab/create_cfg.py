9033faec12efde9add57f1c3cbdc069528397f30
DryRun <david.renhwa.yu@gmail.com>
2016-05-26 01:27:44 +0200

    GENSIM for new samples, resubmit JetHT because crab is dumb

--- gen/crab/create_cfg.py
+++ gen/crab/create_cfg.py
@@ -10,62 +10,57 @@ import sys
 sys.path.append('./')
 
 models = ['GluGluSpin0ToBBbar', 'RSGravitonToBBbar']
-debug = True
+masses = [300, 600, 750, 900, 1200]
 
-def GetDatasetName(model, mass, stage):
+def get_dataset_name(model, mass, stage):
     if model == "GluGluSpin0ToBBbar":
-        base1 = "GluGluSpin0ToBBbar_M_@MASS@_TuneCUEP8M1_13TeV_pythia8"
+        base1 = "GluGluSpin0ToBBbar_M_@MASS@_TuneCUEP8M1_8TeV_pythia8"
     elif model == "RSGravitonToBBbar":
-        base1 = "RSGravitonToBBbar_kMpl01_M_@MASS@_TuneCUEP8M1_13TeV_pythia8"
+        base1 = "RSGravitonToBBbar_kMpl01_M_@MASS@_TuneCUEP8M1_8TeV_pythia8"
     return [base1.replace("@MASS@", str(int(mass))), "dryu-" + stage, "USER"]
 
-def GetCFIPath(model, mass):
-    return "/uscms/home/dryu/Dijets/CMSSW_5_3_32_patch3/src/CMSDIJET/QCDAnalysis/gen/" + GetDatasetName(model, mass, "NULL")[0] + "_cfi.py" 
+def get_CFI_path(model, mass):
+    return "$CMSSW_BASE/src/CMSDIJET/QCDAnalysis/python/" + get_dataset_name(model, mass, "NULL")[0] + "_cfi.py" 
 
-def MakeCFI(model, mass, width):
-    template = open("/uscms/home/dryu/Dijets/CMSSW_5_3_32_patch3/src/CMSDIJET/QCDAnalysis/gen/" + )
-
-def create(mass=750 , width=0.01):
-
-    fin = open( spin+'_cfi.py' , 'r')
-    fout = open( spin.replace('_X_', '_'+str(mass)+'_')+'_cfi.py', 'w')
-
-    for line in fin:
+def make_CFI(model, mass, width=0.01):
+    if model == "GluGluSpin0ToBBbar":
+        template = open("/uscms/home/dryu/Dijets/CMSSW_5_3_32_patch3/src/CMSDIJET/QCDAnalysis/python/GluGluSpin0ToBBbar_M_X_TuneCUEP8M1_8TeV_pythia8_cfi.py.template")
+    elif model == "RSGravitonToBBbar":
+        template = open("/uscms/home/dryu/Dijets/CMSSW_5_3_32_patch3/src/CMSDIJET/QCDAnalysis/python/RSGravitonToBBbar_kMpl01_M_X_TuneCUEP8M1_8TeV_pythia8_cfi.py.template")
+    cfi = open(os.path.expandvars(get_CFI_path(model, mass)), 'w')
+    for line in template:
         if 'm0' in line:
-            fout.write(line.rstrip('\',\n')+("%.0f" % mass)+'\', \n')
+            cfi.write(line.rstrip('\',\n')+("%.0f" % mass)+'\', \n')
         elif 'mWidth' in line:
-            fout.write(line.rstrip('\',\n')+("%.1f" % (mass*width))+'\', \n')
+            cfi.write(line.rstrip('\',\n')+("%.1f" % (mass*width))+'\', \n')
         else:
-            fout.write(line)
+            cfi.write(line)
+    cfi.close()
+    template.close()
 
-    fout.close()
-    fin.close()
-    
-##################
-
-def create_GENSIM_cfg( mass ):
-    command = 'cmsDriver.py Configuration/GenProduction/python/'+(spin.replace('_X_', '_'+str(mass)+'_'))+'_cfi.py'+' --fileout file:GEN-SIM.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot Realistic50ns13TeVCollision --step GEN,SIM --magField 38T_PostLS1 --python_filename GEN-SIM_'+str(mass)+'_cfg.py --no_exec -n 3'
+def create_GENSIM_cfg(model, mass):
+    command = 'cmsDriver.py ' + get_CFI_path(model, mass).replace("$CMSSW_BASE/src/", "")+' --fileout file:GEN-SIM.root --mc --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions auto:mc --beamspot Realistic8TeVCollision --step GEN,SIM --python_filename GEN-SIM_'+model+"_"+str(mass)+'_cfg.py --no_exec -n 3'
     print command
     os.system(command)
 
 #################
 
-def create_GENSIM_crab( mass, unitsPerJob, totalUnits, version=''):
+def create_GENSIM_crab(model, mass, unitsPerJob, totalUnits, version=''):
 
-    fin = open( 'crab_GEN-SIM_cfg.py.ex', 'r')
-    fout = open( 'crab_GEN-SIM_'+str(mass)+'_cfg.py', 'w')
+    fin = open( 'crab_GEN-SIM_cfg.py.template', 'r')
+    fout = open( 'crab_GEN-SIM_'+model + "_" + str(mass)+'_cfg.py', 'w')
 
     for line in fin:
         if 'requestName' in line:
-            fout.write(line.rstrip('\n')+' \'gen-sim_'+spin+'_'+str(mass)+version+'\'\n')
+            fout.write(line.rstrip('\n')+' \'gen-sim_'+model+'_'+str(mass)+version+'\'\n')
         elif 'psetName' in line:
-            fout.write(line.rstrip('\n')+' \'GEN-SIM_'+str(mass)+'_cfg.py\'\n')
+            fout.write(line.rstrip('\n')+' \'GEN-SIM_'+model+"_"+str(mass)+'_cfg.py\'\n')
         elif 'unitsPerJob' in line:
             fout.write(line.rstrip('\n')+' '+str(unitsPerJob)+'\n')
         elif 'totalUnits' in line:
             fout.write(line.rstrip('\n')+' '+str(totalUnits)+'\n')
         elif 'outputPrimaryDataset' in line:
-            fout.write(line.rstrip('\n')+' \''+(spin.replace('_X_', '_'+str(mass)+'_'))+'\'\n')
+            fout.write(line.rstrip('\n')+' \''+(model.replace('_X_', '_'+str(mass)+'_'))+'\'\n')
         elif 'outputDatasetTag' in line:
             fout.write(line.rstrip('\'\n')+version+'\'\n')
         else:
@@ -75,21 +70,24 @@ def create_GENSIM_crab( mass, unitsPerJob, totalUnits, version=''):
 
 #################################
 
-def submit_GENSIM_crab(mass):
-    command = 'crab submit -c '+'crab_GEN-SIM_'+str(mass)+'_cfg.py'
+def submit_GENSIM_crab(model, mass, submit=False):
+    command = 'crab submit -c '+'crab_GEN-SIM_'+model + "_" + str(mass)+'_cfg.py'
     print command
-    if debug:
-        return
-    os.system(command)
+    if submit:
+        os.system(command)
 
 
 #################################
 
-def sequence_GENSIM(mass,version=''):
-    create(mass)
-    create_GENSIM_cfg(mass)
-    create_GENSIM_crab(mass, 1000, 100000, version)
-    submit_GENSIM_crab(mass)
+def sequence_GENSIM(model, mass,version='',submit=False):
+    make_CFI(model, mass)
+    create_GENSIM_cfg(model, mass)
+    if version == "test":
+        n_total = 1000
+    else:
+        n_total = 100000
+    create_GENSIM_crab(model, mass, 1000, n_total, version)
+    submit_GENSIM_crab(model, mass, submit)
 
 #################################
 
@@ -130,10 +128,10 @@ def submit_DR1_crab(mass):
 
 #################################
 
-def sequence_DR1(mass,version=''):
-    create_DR1_cfg(mass)
-    create_DR1_crab(mass, datasets[spin][str(mass)]['DR1'], version)
-    submit_DR1_crab(mass)
+def sequence_DR1(model, mass,version='',submit=False):
+    create_DR1_cfg(model, mass)
+    create_DR1_crab(model, mass, "/" + "/".join(get_dataset_name(model, mass, "DR1")) + "/", version)
+    submit_DR1_crab(model, mass)
 
 #################################
 
@@ -227,18 +225,21 @@ def sequence_MiniAODv2(mass,version=''):
 
 
 # finally, we run:
-for mass in [ 
-    #[650, ''],
-    [750,''], 
-    #[850,''],
-    [1000,'_v2'],
-    [1200,'_v2'],
-    ]:
-    if argv[1]=='GENSIM':
-        sequence_GENSIM(mass[0],mass[1])
-    elif argv[1]=='DR1':
-        sequence_DR1(mass[0],mass[1])
-    elif argv[1]=='DR2':
-        sequence_DR2(mass[0], mass[1])
-    elif argv[1]=='MiniAODv2':
-        sequence_MiniAODv2(mass[0], mass[1])
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description = 'Make and submit CRAB generation jobs')
+    parser.add_argument('version', type=str, help='Version')
+    parser.add_argument('--submit', action='store_true', default=False, help='Submit jobs after creation')
+    parser.add_argument('--GENSIM', action='store_true', help='')
+    parser.add_argument('--DR1', action='store_true', help='')
+    parser.add_argument('--DR2', action='store_true', help='')
+    args = parser.parse_args()
+
+    for model in models:
+        for mass in masses:
+            if args.GENSIM:
+                sequence_GENSIM(model, mass, args.version, submit=args.submit)
+            elif args.DR1:
+                sequence_DR1(model, mass, args.version, submit=args.submit)
+            elif args.DR2:
+                sequence_DR2(model, mass, args.version, submit=args.submit)
