a04df0f69f1f35a234c6b4d74518531cb2174845
DryRun <david.renhwa.yu@gmail.com>
2016-07-28 11:56:00 +0200

    Sync

--- python/singlemu_trigger_plot.py
+++ python/singlemu_trigger_plot.py
@@ -25,72 +25,30 @@ def f8(seq): # Dave Kirby
 if __name__ == "__main__":
 
 	for analysis in ["lowmass", "highmass"]:
-		ht_analyses = {}
-		names = []
-		for mass in xrange(250, 700, 50):
-			if mass == 600:
-				continue
-			if mass == 650:
-				continue
-			names.append("HT" + str(mass))
-			if sr == "highmass":
-				ht_analyses["HT" + str(mass)] = "trigjetht" + str(mass) + "_CSVTM"
-			else:
-				ht_analyses["HT" + str(mass)] = "trigjetht" + str(mass) + "_eta1p7_CSVTM"
-
-		# For now, only the high mass SR has the unprescaled JetHT triggers
-		if sr == "highmass":
-			names.append("HTUnprescaled")
-			ht_analyses["HTUnprescaled"] = "trigjetht_CSVTM"
-		ranges = {
-			"HT250":[400, 475],
-			"HT300":[475, 525],
-			"HT350":[525, 575],
-			"HT400":[575, 625],
-			"HT450":[625, 700],
-			"HT500":[700, 750],
-			"HT550":[750, 900],
-			#"HT650":[800, 900],
-			"HTUnprescaled":[900, 2000]
-		}
-		boundaries = []
-		for name, interval in ranges.iteritems():
-			boundaries.append(interval[0])
-			boundaries.append(interval[1])
-		boundaries = f8(boundaries)
-		boundaries.sort()
-
-		jetht_sample = "JetHT_2012BCD"
-		histograms = {}
-		for name in names:
-			f = TFile(analysis_config.get_b_histogram_filename(ht_analyses[name], jetht_sample), "READ")
-			#histograms[name] = mjj_common.apply_dijet_binning_normalized(f.Get("BHistograms/h_pfjet_mjj"))
-			print "[debug] For name " + name + ", input events = " + str(f.Get("BHistograms/h_input_nevents").GetEntries())
-			print "[debug] \tPrescale = " + str(f.Get("BHistograms/h_pass_nevents_weighted").GetBinContent(1) / f.Get("BHistograms/h_pass_nevents").GetBinContent(1))
-			histograms[name] = f.Get("BHistograms/h_pfjet_mjj")
-			histograms[name].SetName(histograms[name].GetName() + "_" + name)
-			histograms[name].SetDirectory(0)
-			f.Close()
-		jetht_histogram = jetht_frankenhist(names, histograms, ranges)
-		jetht_histogram.Rebin(25)
-
-		if sr == "highmass":
-			f_bjetplusx = TFile(analysis_config.get_b_histogram_filename("trigbbh_CSVTM", "BJetPlusX_2012BCD"), "READ")
+		if analysis == "highmass":
+			f_bjetplusx = TFile(analysis_config.get_b_histogram_filename("trigbbh_CSVTM", "BJetPlusX_2012"), "READ")
+			f_singlemu = TFile(analysis_config.get_b_histogram_filename("mu_highmass_CSVTM", "SingleMu_2012"), "READ")
 		else:
 			f_bjetplusx = TFile(analysis_config.get_b_histogram_filename("trigbbl_CSVTM", "BJetPlusX_2012BCD"), "READ")
-		print "[debug] For BJetsPlusX_2012BCD, input events = " + str(f_bjetplusx.Get("BHistograms/h_input_nevents").GetEntries())
+			f_singlemu = TFile(analysis_config.get_b_histogram_filename("mu_lowmass_CSVTM", "SingleMu_2012"), "READ")
+		print "[debug] For BJetsPlusX_2012, input events = " + str(f_bjetplusx.Get("BHistograms/h_input_nevents").GetEntries())
+		print "[debug] For SingleMu_2012, input events = " + str(f_singlemu.Get("BHistograms/h_input_nevents").GetEntries())
 		bjetplusx_histogram = f_bjetplusx.Get("BHistograms/h_pfjet_mjj").Rebin(25)
 		bjetplusx_histogram.SetDirectory(0)
 		f_bjetplusx.Close()
+		singlemu_histogram = f_singlemu.Get("BHistograms/h_pfjet_mjj").Rebin(25)
+		singlemu_histogram.SetDirectory(0)
+		f_singlemu.Close()
 
-		plot = AnalysisComparisonPlot(bjetplusx_histogram, jetht_histogram, "BJetPlusX", "JetHT", "online_btag_efficiency_" + sr, x_range=[0., 1200.], log=True)
+		# Normalize the histograms above 450 GeV
+		norm_low_bin = bjetplusx_histogram.GetXaxis().FindBin(450)
+		norm_high_bin = bjetplusx_histogram.GetXaxis().GetNbins()
+		singlemu_histogram.Scale(bjetplusx_histogram.Integral(norm_low_bin, norm_high_bin) / singlemu_histogram.Integral(norm_low_bin, norm_high_bin))
+
+		plot = AnalysisComparisonPlot(bjetplusx_histogram, singlemu_histogram, "BJetPlusX", "SingleMu", "online_btag_efficiency_mu_" + analysis, x_range=[0., 1200.], log=True)
 		plot.draw()
 		plot.top.cd()
-		ymin = plot.frame_top.GetMinimum()
-		ymax = plot.frame_top.GetMaximum()
-		for boundary in boundaries:
-			plot.draw_line("top", boundary, ymin, boundary, ymax)
-			plot.draw_line("bottom", boundary, -0.2, boundary, 1.2)
+
 		# Fit constant to ratio
 		ratio_fit = TF1("ratio_fit", "[0]", 400, 1000)
 		plot.hist_ratio.Fit(ratio_fit, "QR0")
